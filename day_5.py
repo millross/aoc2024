7229518434f605758b963968f58b1d1cf0d63df3
Jeremy Prime <jeremy_prime@yahoo.co.uk>
2024-12-05 20:32:19 +0000

    Day 5 Part 1 Done

--- day_5.py
+++ day_5.py
@@ -2,8 +2,10 @@ from operator import truediv
 
 from common.aoc_2024_common import load_file, group_by_delimiter
 import itertools as it
+from math import floor
 
 TEST_FILE_NAME: str = "day5_test_input.txt"
+ACTUAL_FILE_NAME: str = "day5_actual_input.txt"
 
 def load_file_inputs(file_name: str) -> [[str], [str]]:
     groups: [str] = group_by_delimiter(load_file(file_name), "")
@@ -32,6 +34,7 @@ class OrderingSpecification:
         self.specification = {}
         for k, g in it.groupby(ordering_rules, lambda r: r.before):
             self.specification[k] = set(map(lambda r: r.after, g))
+        print(self.specification)
 
     def is_compliant(self, earlier: int, later: int):
         # for x in self.specification:
@@ -46,15 +49,30 @@ def page_ordering_complies_with_specification(ordering: [int], specification: Or
     for earlier,later in zip(ordering, ordering[1:]):
         if not specification.is_compliant(earlier=earlier, later=later):
             return False
-        return True
+    return True
+
+def convert_ordering_str_to_list(ordering: str) -> [int]:
+    return list(map(int, ordering.split(",")))
+
+def middle_page_number(ordering: [int]) -> int:
+    return ordering[floor(len(ordering) / 2)]
 
 def part_1(file_name):
     ordering_rules: str
-    page_orderings: str
+    page_orderings_strs: str
+    [ordering_rules, page_orderings_strs] = load_file_inputs(file_name)
+    page_orderings: [[int]] = list(map(convert_ordering_str_to_list, page_orderings_strs))
+    ordering_specification: OrderingSpecification = OrderingSpecification(list(map(OrderingRuleDefinition, ordering_rules)))
+    compliant_orderings = filter(lambda o: page_ordering_complies_with_specification(o, ordering_specification), page_orderings)
+    middle_page_numbers = map(middle_page_number, compliant_orderings)
+    return sum(middle_page_numbers)
+
+
+
+def test_compliance():
     [ordering_rules, page_orderings] = load_file_inputs(TEST_FILE_NAME)
     ordering_specification: OrderingSpecification = OrderingSpecification(list(map(OrderingRuleDefinition, ordering_rules)))
-    print("Compliant orderings are as follows:")
-    for ordering in filter(lambda o: page_ordering_complies_with_specification(o, ordering_specification), page_orderings):
-        print(ordering)
+    print(ordering_specification.is_compliant(earlier=13, later=29))
 
-part_1(TEST_FILE_NAME)
+print("Part 1 test result: " + str(part_1(TEST_FILE_NAME)))
+print("Part 1 actual result: " + str(part_1(ACTUAL_FILE_NAME)))
